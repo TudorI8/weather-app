71df10c14fadcd8bf4fe5f7c035f00b61a54cd1f
Tudor Istrate <tud8r.istrate@gmail.com>
2024-08-26 15:44:35 +0300

    feat: show forecast weather onclick showForecastBtn

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const showWeatherBtn = document.getElementById('show-weather');
 const cityInput = document.getElementById('city');
+const showWeatherBtn = document.getElementById('show-weather');
+const showForecastBtn = document.getElementById('show-forecast');
 const weatherContainer = document.getElementById('weather-container');
+const forecastContainer = document.getElementById('forecast-container');
 
 showWeatherBtn.addEventListener('click', showWeather);
-
 cityInput.addEventListener('keypress', function(e) {
 	if (e.key === 'Enter') {
 		showWeather();
 	}
 });
+showForecastBtn.addEventListener('click', showForecast);
 
 const URL_CURRENT_WEATHER =
 	'https://api.openweathermap.org/data/2.5/weather?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=';
+const URL_FORECAST_WEATHER =
+  'https://api.openweathermap.org/data/2.5/forecast?appid=69518b1f8f16c35f8705550dc4161056&units=metric&q=';
 
 async function showWeather() {
 	const city = cityInput.value;
@@ -32,4 +36,28 @@ async function showWeather() {
          <p>Today's Minimum: ${weather.main.temp_min} </p>
       </div>
    `;
+}
+
+async function showForecast() {
+  const city = cityInput.value;
+  const response = await fetch(`${URL_FORECAST_WEATHER}${city}`);
+  const forecast = await response.json();
+
+  const forecasts = forecast.list;
+
+  forecastContainer.innerHTML = forecasts
+    .map(entry => {
+      const iconCode = entry.weather[0].icon;
+      const iconImageUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+      const time = new Date(entry.dt * 1000).toLocaleTimeString();
+
+      return `
+        <div>
+          <p>Time: ${time}</p>
+          <p>Temperature: ${entry.main.temp} °C</p>
+          <p>Description: ${entry.weather[0].description}</p>
+          <img src=${iconImageUrl} alt="${entry.weather[0].description}" />
+        </div>
+      `;
+    }).join('');
 }
